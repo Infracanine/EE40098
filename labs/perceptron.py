373a0c09084887a3d549dcdb25bfa3361891a3a5
Samuel Taylor-Hansen <samueltaylorhansen@gmail.com>
2021-10-18 11:02:41 +0100

    initial perceptron class and MVP  str output

--- labs/perceptron.py
+++ labs/perceptron.py
@@ -7,7 +7,7 @@ class Perceptron():
         self.bias = bias
         
     # Apply perceptron to some list of inputs
-    def apply(inputs_list):
+    def apply(self, inputs_list):
         inputs = np.array(inputs_list)
 
         # Calculate the dot product
@@ -21,9 +21,12 @@ class Perceptron():
         output = 1 if summed > 0 else 0
         return output
 
-    def __str__():
-        for i in range()
-        return f""
+    def __str__(self):
+        weights_output = ""
+        for i in range(self.weights.size):
+            weights_output += f"{self.weights[i]}x_{i} + "
+        return weights_output + str(self.bias) + " = y"
 
 if __name__ == "__main__":
-    
+    p = Perceptron([1.0, 2.0], -1)
+    print(str(p))
